9ff8359175a2e551681aed1e272ccc2fbb70dc36
Beth C <147827249+beth-cl@users.noreply.github.com>
2024-11-26 11:55:16 +0000

    not much stuff

--- All Dogs go to heaven V3/Assets/scripts/digspots/text appear on screen.cs
+++ All Dogs go to heaven V3/Assets/scripts/digspots/text appear on screen.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class textappearonscreen : MonoBehaviour
 {
+    public TextMeshProUGUI textMeshPro;
     // Start is called before the first frame update
     void Start()
     {
-        
+        textMeshPro.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void checkcollision()
+    {
+
     }
 }
